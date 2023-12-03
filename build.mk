72ea9349a6a4641b5cfbb1e1e34b2d92375d0ea4
Eugene Ciurana <github@cime.net>
2023-12-03 07:56:21 -0800

    Implemented and tested in the update() call

--- build.mk
+++ build.mk
@@ -1,3 +1,5 @@
+# License:  https://raw.githubusercontent.com/pr3d4t0r/lucyfer/master/LICENSE.txt
+
 LUCY_PRESENT=$(shell if [[ -e "lucy" ]]; then echo "true"; else echo "false"; fi)
 
 image:
@@ -7,14 +9,7 @@ ifeq ($(LUCY_PRESENT), true)
 	awk -v "version=$(DOCKER_VERSION)" '/^LUCYFER_HUB_VERSION/ { printf("LUCYFER_HUB_VERSION=\"%s\"\n", version); next; } { print; }' lucy > /tmp/lucy && \
     cat /tmp/lucy > lucy && \
     rm /tmp/lucy
-endif
-
-
-build:
-	docker buildx build --platform linux/amd64,linux/arm64 --compress --force-rm --progress=plain -t $(DOCKER_IMAGE):$(DOCKER_VERSION) --build-arg LUCYFER_VERSION=$(DOCKER_VERSION) --push .
-	docker buildx imagetools create -t $(DOCKER_IMAGE):latest $(DOCKER_IMAGE):$(DOCKER_VERSION)
-ifeq ($(LUCY_PRESENT), true)
-	awk -v "version=$(DOCKER_VERSION)" '/^LUCYFER_HUB_VERSION/ { printf("LUCYFER_HUB_VERSION=\"%s\"\n", version); next; } { print; }' lucy > /tmp/lucy && \
+	awk -v "version=$(DOCKER_VERSION)" '/^KALLISTO_HUB_VERSION/ { printf("KALLISTO_HUB_VERSION=\"%s\"\n", version); next; } { print; }' lucy > /tmp/lucy && \
     cat /tmp/lucy > lucy && \
     rm /tmp/lucy
 endif
